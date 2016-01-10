83cf8e7e54cca21be3a305aed77e206a5beabfaa
tuna <tunataskintuna35@gmail.com>
2016-01-10 12:38:33 +0200

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,1 +1,45 @@
+/* this project is designed by Tuna & Elif*/
+/* Date 10.01.2016 */
+ 
+char* alphabet[26] = { 
+"a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z" 
+};
+void setup()
+{
+  Serial.begin(9600);
+}
 
+void loop()
+{
+  for(int i=0;i<=26;i++)
+    {
+      serial.println(alphabet[i]);
+      delay(100);
+    }
+  
+}
