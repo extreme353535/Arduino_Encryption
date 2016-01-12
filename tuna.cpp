6068ed0d38bf0201c5f02b38261e956ab0173bfa
tuna <tunataskintuna35@gmail.com>
2016-01-12 16:41:39 +0200

    Update tuna.cpp

--- tuna.cpp
+++ tuna.cpp
@@ -1,1 +1,9 @@
-
+String stringOne = "123456789";
+char charBuf[50];
+stringOne.toCharArray(charBuf, 50) ;
+Serial.println(stringOne);
+for (int i = 0; i < 9; i++) {
+  Serial.print(" 0x");
+  Serial.print(charBuf[i], HEX);
+}
+Serial.println();
